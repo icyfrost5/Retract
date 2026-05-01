9e86014f6f6bedbbd4af164e043620286c320c40
Senay K <157552727+icyfrost5@users.noreply.github.com>
2026-04-30 23:43:23 -0400

    Refactor sidebar and index functions in Retract.py
    Fixed sidebar functionality, updated the index page so that it doesn't collide with anything, changed the color scheme of the pages in light/dark mode, and added an images page

--- Retract/Retract/Retract.py
+++ Retract/Retract/Retract.py
@@ -2,57 +2,62 @@ import asyncio
 import reflex as rx
 
 
-
 class State(rx.State):
-    Loading = True
-    colormodeicon = "moon_star"
+    pass
 
-    async def skeleton_wait_time(self):
-        self.Loading = True
-        await asyncio.sleep(0.2)
-        self.Loading = False
-
-
-@rx.page(on_load=State.skeleton_wait_time)
-def index():
-    # Welcome Page (Index)
-    return rx.hstack(
-        sidebar(),
-        rx.box(
-            rx.skeleton(rx.color_mode.button(position="top-right"), loading=State.Loading),
-            margin_left="200px"
-        )
-    )
-
-
-def sidebar_link(text, icon, url, ):
-    return rx.link(rx.hstack(rx.icon(icon, color=rx.color_mode_cond(light="rgb(0, 0, 0)", dark="rgb(255, 255, 255)")),
-                             rx.text(text, size="5",
-                             color=rx.color_mode_cond(light="rgb(0, 0, 0)", dark="rgb(255, 255, 255)")),
-                             align="center", width="100%", style={
-                                "_hover": {"bg": rx.color_mode_cond(light="rgb(220, 220, 220)", dark="rgb(50, 50, 50)")}}), href=url)
-
+def sidebar_link(text, icon, url, size, width):
+    return rx.link(rx.hstack(rx.icon(icon, color=rx.color_mode_cond(light="#0A0F1E", dark="#C8A96E")),
+                             rx.text(text, size=size,
+                             color=rx.color_mode_cond(light="#0A0F1E", dark="#C8A96E")),
+                             align="center", width=width, style={
+                                "_hover": {"bg": rx.color_mode_cond(light="#d8f2fa", dark="#282828")}}), href=url)
 
 def sidebar():
     return rx.box(
         rx.vstack(
             rx.vstack(
-                sidebar_link("Home", "layout-dashboard", "/"),
+                sidebar_link("Home", "layout-dashboard", "/", '5', "130%"),
+                sidebar_link("Image Generator", "image_plus", "/images", '4', "100%"),
                 spacing="5",
                 align="start"
             ),
             spacing="5",
-            bg=rx.color_mode_cond(light="rgb(240, 240, 240)", dark="rgb(27, 27, 27)"),
+            bg=rx.color_mode_cond(light="#bfe9f7", dark="#1e1e1e"),
             align="start",
             position="fixed",
             padding_y=".75em",
             padding_x="1em",
             justify = "start",
             flex_wrap = "wrap",
-            width="200px"
+            width="150px",
+            height="100vh"
+        )
+    )
+
+def index():
+    # Welcome Page (Index)
+    return rx.hstack(
+        sidebar(),
+        rx.box(
+            rx.color_mode.button(position="top-right",color=rx.color_mode_cond(light="#0A0F1E", dark="#C8A96E")),
+            bg=rx.color_mode_cond(light="#cceffa", dark="#111111"),
+            width="100%",
+            height="100vh"
+        ),
+    )
+def images():
+    return rx.hstack(
+        sidebar(),
+        rx.box(
+            rx.button(icon="settings", position="top-right",),
+            bg=rx.color_mode_cond(light="#cceffa", dark="#111111"),
+            width="100%",
+            height="100vh"
         )
     )
 
 
+
 app = rx.App()
-app.add_page(index)
+app.add_page(index, "/")
+app.add_page(images, "/images")
